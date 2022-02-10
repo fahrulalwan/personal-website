37bbaacfd280279c465e418964d6f7e06b367b81
Mohammad Fahrul Alwan <fahrulalwan@gmail.com>
2022-02-10 09:53:57 +0700

    fix discarded code before upgrade repo

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,15 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import remark from 'remark';
+import { remark } from 'remark';
 import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-export function getSortedPostsData() {
+export function getSortedPostsData(): { date: string; id: string; title: string }[] {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
+  const allPostsData = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '');
 
@@ -35,16 +35,16 @@ export function getSortedPostsData() {
   });
 }
 
-export function getAllPostIds() {
+export function getAllPostIds(): { params: { id: string } }[] {
   const fileNames = fs.readdirSync(postsDirectory);
-  return fileNames.map((fileName) => ({
+  return fileNames.map(fileName => ({
     params: {
       id: fileName.replace(/\.md$/, ''),
     },
   }));
 }
 
-export async function getPostData(id: string) {
+export async function getPostData(id: string): Promise<{ id: string; contentHtml: string }> {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
@@ -52,9 +52,7 @@ export async function getPostData(id: string) {
   const matterResult = matter(fileContents);
 
   // Use remark to convert markdown into HTML string
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
+  const processedContent = await remark().use(html).process(matterResult.content);
   const contentHtml = processedContent.toString();
 
   // Combine the data with the id and contentHtml
