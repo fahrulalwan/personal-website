deb463fd5877ff55f099fc4d6cea77b1ddb5e4a0
fahrulalwan <fahrulalwan@gmail.com>
2024-11-18 15:36:17 +0700

    fix: disable automatic Vercel monitors and server function instrumentation

--- next.config.ts
+++ next.config.ts
@@ -40,5 +40,8 @@ disableLogger: true,
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+// automaticVercelMonitors: true,
+
+  autoInstrumentServerFunctions: false,
+  // autoInstrumentMiddleware: false,
 });
